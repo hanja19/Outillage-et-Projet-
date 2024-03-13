fbe114483a9e17fd03c00974117bcb9a46b53e7b
hanja19 <hanjarj19@gmail.com>
2024-03-13 21:47:43 +0100

    TEST 1

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module bigbrain.outillageetprojet {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens bigbrain.outillageetprojet to javafx.fxml;
+    exports bigbrain.outillageetprojet;
+}
